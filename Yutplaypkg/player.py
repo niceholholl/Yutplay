f31143845aa27181dc17c9d68ee19bbffc740d78
niceholholl <165501701+niceholholl@users.noreply.github.com>
2024-12-09 05:26:15 +0900

    Update player.py

--- Yutplaypkg/player.py
+++ Yutplaypkg/player.py
@@ -10,13 +10,6 @@ class Player(GameComponent):
         self.mal_symbols = mal_symbols
         print(f"Initializing Player with name: {name} and mal_symbols: {mal_symbols}")    
         self.mals = [Mal(name, symbol, verbose) for symbol in mal_symbols]
-        try:
-            from Yutplaypkg.mal import Mal
-            print("Mal class imported successfully")
-        except ImportError as e:
-            print(f"Failed to import Mal: {e}")
-
-
 
     def get_active_mals(self):
         return [mal for mal in self.mals if not mal.is_finished]
