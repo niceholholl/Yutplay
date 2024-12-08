73f52f731236b4fbde1e94b243e3739955eb9f91
niceholholl <165501701+niceholholl@users.noreply.github.com>
2024-12-09 05:03:44 +0900

    Update yut.py

--- Yutplaypkg/yut.py
+++ Yutplaypkg/yut.py
@@ -1,6 +1,12 @@
 # yut.py
 
 from Yutplaypkg.gamecomponent import GameComponent
+from Yutplaypkg.setmal import SetMal
+from Yutplaypkg.mal import Mal
+from Yutplaypkg.player import Player
+from Yutplaypkg.yutpan import Yutpan
+from Yutplaypkg.gamestart import Gamestart
+from Yutplaypkg.myturn import Myturn
 
 class Yut(GameComponent):
     def __init__(self, verbose=False):
