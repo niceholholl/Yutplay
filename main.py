1613834d6e87724bb205587db68760a762f637d7
niceholholl <165501701+niceholholl@users.noreply.github.com>
2024-12-09 13:24:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # main.py
 
-from Yutplaypkg import Gamestart, Yutpan, Myturn
+from Yutplaypkg.gamestart import Gamestart
+from Yutplaypkg.yutpan import Yutpan
+from Yutplaypkg.myturn import Myturn
 
+# 메인 함수 - 게임 실행
 def main():
-    verbose = False  # 디버그 출력을 원하지 않으면 False로 설정
+    verbose = False
     game = Gamestart(verbose)
     game.set_mal()
     yutpan = Yutpan(verbose)
@@ -12,14 +15,14 @@ def main():
     print('(참고 : 윷판에 해당하는 인덱스는 좌측 상단부터 우측 하단 순서로 실행됩니다!)')
 
     while True:
-        print(f"\n{game.current_player}님의 차례입니다.")
+        print(f"{game.current_player}님의 차례입니다.")
         yutpan.print_board(game.players_mal_dict)
 
         turn = Myturn(game, yutpan, verbose)
         result = turn.turn_process()
 
         if result == "WIN":
-            print(f"축하합니다 {game.current_player}님! 게임에서 승리하셨습니다!")
+            print(f"{game.current_player}님이 게임에서 승리하셨습니다! 축하드립니다!")
             break
         elif result in [None, "CONTINUE"]:
             game.switch_player()
