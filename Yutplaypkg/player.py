ba9555bd179a4feb84c1ca57a513880861dbfc50
niceholholl <165501701+niceholholl@users.noreply.github.com>
2024-12-09 09:27:36 +0900

    Update player.py

--- Yutplaypkg/player.py
+++ Yutplaypkg/player.py
@@ -8,12 +8,7 @@ class Player(GameComponent):
         super().__init__(verbose)
         self.name = name
         self.mal_symbols = mal_symbols
-        print(f"[DEBUG] Initializing Player with name: {name}, mal_symbols: {mal_symbols}")
-        # 문제 발생 지점: Mal 클래스 생성 시 문제 확인
         self.mals = []
-        for symbol in mal_symbols:
-            print(f"[DEBUG] Creating Mal object for symbol: {symbol}")
-            self.mals.append(Mal(name, symbol, verbose))
-
+        
     def get_active_mals(self):
         return [mal for mal in self.mals if not mal.is_finished]
