c48093e41255fee8c6cc3e68781bf31e840c3a7e
niceholholl <165501701+niceholholl@users.noreply.github.com>
2024-12-15 15:02:43 +0900

    Update player.py

--- Yutplaypkg/player.py
+++ Yutplaypkg/player.py
@@ -8,7 +8,7 @@ class Player(GameComponent):
         super().__init__(verbose)
         self.name = name
         self.mal_symbols = mal_symbols
-        self.mals = []
-        
+        self.mals = [Mal(name, symbol, verbose) for symbol in mal_symbols]
+
     def get_active_mals(self):
         return [mal for mal in self.mals if not mal.is_finished]
